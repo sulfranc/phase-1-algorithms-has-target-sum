7bd7b2aed4dd86cb8d43d3951d68e5b5efca3d72
Jeff Sulfranc <sulfranc@gmail.com>
2022-02-16 22:07:07 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Write your algorithm here\
+  for(i = 0; i < array.length - 1; i++){
+    for (j = i + 1; j < array.length; j++){
+      if (target == array[j] + array[i]){
+        return true
+
+        
+      }
+      
+      
+    }
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here\
+  form a while loop
+  have while loop , loop through array and add 2num
+  make an if statement to see if sum of 2num = targert number
 */
 
 /*
   Add written explanation of your solution here
+    this 
 */
 
 // You can run `node index.js` to view these console logs
